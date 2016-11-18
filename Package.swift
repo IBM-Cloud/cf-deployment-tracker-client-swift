4756f4ab5aea30f2a5c2d03369fa994a3acf61a1
Brian Hernandez <bhernand@us.ibm.com>
2016-11-18 12:18:55 -0600

    Updating packaging dependency for Kitura-net

--- Package.swift
+++ Package.swift
@@ -27,6 +27,6 @@ let package = Package(
     dependencies: [
       .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 1, minor: 8),
       .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 1),
-      .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1, minor: 1)
+      .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1, minor: 2)
     ]
 )
