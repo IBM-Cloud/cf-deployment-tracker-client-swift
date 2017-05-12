8f76cf4eba18cfd928b9af6034b890a2c426755f
Ricardo Olivieri <rolivieri@users.noreply.github.com>
2017-05-12 14:30:57 -0500

    Removed dependency on kitura-net. (#46)
    * Removed dependency on kitura-net.
    
    * Removed dependency on kitura-net.
    
    * Removed dependency on kitura-net.
    
    * Removed dependency on kitura-net.

--- Package.swift
+++ Package.swift
@@ -26,7 +26,6 @@ let package = Package(
     ],
     dependencies: [
       .Package(url: "https://github.com/IBM-Swift/CloudConfiguration.git", majorVersion: 2),
-      .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", majorVersion: 1),
-      .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1)
+      .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", majorVersion: 1)
     ]
 )
--- Sources/CloudFoundryDeploymentTracker/DeploymentTracker.swift
+++ Sources/CloudFoundryDeploymentTracker/DeploymentTracker.swift
@@ -19,10 +19,8 @@ import Configuration
 import CloudFoundryEnv
 import CloudFoundryConfig
 import LoggerAPI
-import KituraNet
 
 public struct CloudFoundryDeploymentTracker {
-
   let configMgr: ConfigurationManager
   let repositoryURL: String
   var codeVersion: String?
@@ -41,38 +39,45 @@ public struct CloudFoundryDeploymentTracker {
 
   /// Sends off HTTP post request to tracking service, simply logging errors on failure
   public func track() {
-    Log.verbose("About to construct http request for cf-deployment-tracker-service...")
+    Log.verbose("About to construct HTTP request for cf-deployment-tracker-service...")
     if let trackerJson = buildTrackerJson(configMgr: configMgr),
-       let jsonData = try? JSONSerialization.data(withJSONObject: trackerJson) {
+    let jsonData = try? JSONSerialization.data(withJSONObject: trackerJson) {
+      let jsonStr = String(data: jsonData, encoding: .utf8)
+      Log.verbose("JSON payload for cf-deployment-tracker-service is: \(String(describing: jsonStr))")
+      // Build URL instance
+      guard let url = URL(string: "https://deployment-tracker.mybluemix.net:443/api/v1/track") else {
+        Log.verbose("Failed to create URL object to connect to cf-deployment-tracker-service...")
+        return
+      }
+      var request = URLRequest(url: url)
+      request.httpMethod = "POST"
+      request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+      request.httpBody = jsonData
 
-      Log.verbose("JSON payload for cf-deployment-tracker-service is: \(jsonData)")
-      var requestOptions: [ClientRequest.Options] = []
-      requestOptions.append(.method("POST"))
-      requestOptions.append(.schema("https://"))
-      requestOptions.append(.hostname("deployment-tracker.mybluemix.net"))
-      requestOptions.append(.port(443))
-      requestOptions.append(.path("/api/v1/track"))
-      let headers = ["Content-Type": "application/json"]
-      requestOptions.append(.headers(headers))
-      Log.verbose("Successfully built HTTP request options for cf-deployment-tracker-service...")
+      // Build task for request
+      let requestTask = URLSession(configuration: .default).dataTask(with: request) {
+        data, response, error in
 
-      let req = HTTP.request(requestOptions) { response in
-        if let response = response, response.statusCode == HTTPStatusCode.OK || response.statusCode == HTTPStatusCode.created {
-          Log.info("Uploaded stats \(response.status)")
-          do {
-            var body = Data()
-            try response.readAllData(into: &body)
-            let jsonResponse = try JSONSerialization.jsonObject(with: body, options: [])
-            Log.info("Deployment Tracker response: \(jsonResponse)")
-          } catch {
-            Log.error("Bad JSON doc received from deployment tracker.")
+        guard let httpResponse = response as? HTTPURLResponse else {
+          Log.error("Failed to send tracking data to cf-deployment-tracker-service: \(String(describing: error))")
+          return
+        }
+
+        Log.info("HTTP response code: \(httpResponse.statusCode)")
+        // OK = 200, CREATED = 201
+        if httpResponse.statusCode == 200 || httpResponse.statusCode == 201 {
+          if let data = data, let jsonResponse = try? JSONSerialization.jsonObject(with: data, options: []) {
+            Log.info("cf-deployment-tracker-service response: \(jsonResponse)")
+          } else {
+            Log.error("Bad JSON payload received from cf-deployment-tracker-service.")
           }
         } else {
-          Log.error("Failed to send tracking data with status code: \(String(describing: response?.status))")
+          Log.error("Failed to send tracking data to cf-deployment-tracker-service.")
         }
       }
-      req.end(jsonData)
-      Log.verbose("Successfully sent HTTP request to cf-deployment-tracker-service...")
+      Log.verbose("Successfully built HTTP request options for cf-deployment-tracker-service.")
+      requestTask.resume()
+      Log.verbose("Sent HTTP request to cf-deployment-tracker-service...")
     } else {
       Log.verbose("Failed to build valid JSON payload for deployment tracker... maybe running locally and not on the cloud?")
     }
@@ -84,64 +89,64 @@ public struct CloudFoundryDeploymentTracker {
   ///
   /// - returns: JSON, assuming we have access to application info
   public func buildTrackerJson(configMgr: ConfigurationManager) -> [String:Any]? {
-      var jsonEvent: [String:Any] = [:]
-      guard let vcapApplication = configMgr.getApp() else {
-        Log.verbose("Couldn't get Cloud Foundry App instance... maybe running locally and not on the cloud?")
-        return nil
-      }
+    var jsonEvent: [String:Any] = [:]
+    guard let vcapApplication = configMgr.getApp() else {
+      Log.verbose("Couldn't get Cloud Foundry App instance... maybe running locally and not on the cloud?")
+      return nil
+    }
 
-      Log.verbose("Preparing dictionary payload for cf-deployment-tracker-service...")
-      let dateFormatter = DateFormatter()
-      #if os(OSX)
-        //dateFormatter.calendar = Calendar(identifier: .iso8601)
-        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-        dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
-      #else
-        //dateFormatter.calendar = Calendar(identifier: .iso8601)
-        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-        dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
-      #endif
-      dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSX"
-      jsonEvent["date_sent"] = dateFormatter.string(from: Date())
+    Log.verbose("Preparing dictionary payload for cf-deployment-tracker-service...")
+    let dateFormatter = DateFormatter()
+    #if os(OSX)
+    //dateFormatter.calendar = Calendar(identifier: .iso8601)
+    dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+    dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+    #else
+    //dateFormatter.calendar = Calendar(identifier: .iso8601)
+    dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+    dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
+    #endif
+    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSX"
+    jsonEvent["date_sent"] = dateFormatter.string(from: Date())
 
-      if let codeVersion = self.codeVersion {
-        jsonEvent["code_version"] = codeVersion
-      }
-      jsonEvent["repository_url"] = repositoryURL
-      jsonEvent["runtime"] = "swift"
-      jsonEvent["application_name"] = vcapApplication.name
-      jsonEvent["space_id"] = vcapApplication.spaceId
-      jsonEvent["application_id"] = vcapApplication.id
-      jsonEvent["application_version"] = vcapApplication.version
-      jsonEvent["application_uris"] = vcapApplication.uris
-      jsonEvent["instance_index"] = vcapApplication.instanceIndex
+    if let codeVersion = self.codeVersion {
+      jsonEvent["code_version"] = codeVersion
+    }
+    jsonEvent["repository_url"] = repositoryURL
+    jsonEvent["runtime"] = "swift"
+    jsonEvent["application_name"] = vcapApplication.name
+    jsonEvent["space_id"] = vcapApplication.spaceId
+    jsonEvent["application_id"] = vcapApplication.id
+    jsonEvent["application_version"] = vcapApplication.version
+    jsonEvent["application_uris"] = vcapApplication.uris
+    jsonEvent["instance_index"] = vcapApplication.instanceIndex
 
-      Log.verbose("Verifying services bound to application...")
-      let services = configMgr.getServices()
-      if services.count > 0 {
-        var serviceDictionary = [String: Any]()
-        for (_, service) in services {
-            if var serviceStats = serviceDictionary[service.label] as? [String: Any] {
-            if let count = serviceStats["count"] as? Int {
-                serviceStats["count"] = count + 1
-            }
-            if var plans = serviceStats["plans"] as? [String] {
-              if !plans.contains(service.plan) { plans.append(service.plan) }
-              serviceStats["plans"] = plans
-            }
-            serviceDictionary[service.label] = serviceStats
-          } else {
-            var newService = [String: Any]()
-            newService["count"] = 1
-            newService["plans"] = service.plan.components(separatedBy: ", ")
-            serviceDictionary[service.label] = newService
+    Log.verbose("Verifying services bound to application...")
+    let services = configMgr.getServices()
+    if services.count > 0 {
+      var serviceDictionary = [String: Any]()
+      for (_, service) in services {
+        if var serviceStats = serviceDictionary[service.label] as? [String: Any] {
+          if let count = serviceStats["count"] as? Int {
+            serviceStats["count"] = count + 1
           }
+          if var plans = serviceStats["plans"] as? [String] {
+            if !plans.contains(service.plan) { plans.append(service.plan) }
+            serviceStats["plans"] = plans
+          }
+          serviceDictionary[service.label] = serviceStats
+        } else {
+          var newService = [String: Any]()
+          newService["count"] = 1
+          newService["plans"] = service.plan.components(separatedBy: ", ")
+          serviceDictionary[service.label] = newService
         }
-        jsonEvent["bound_vcap_services"] = serviceDictionary
       }
-      Log.verbose("Finished preparing dictionary payload for cf-deployment-tracker-service.")
-      Log.verbose("Dictionary payload for cf-deployment-tracker-service is: \(jsonEvent)")
-      return jsonEvent
+      jsonEvent["bound_vcap_services"] = serviceDictionary
+    }
+    Log.verbose("Finished preparing dictionary payload for cf-deployment-tracker-service.")
+    Log.verbose("Dictionary payload for cf-deployment-tracker-service is: \(jsonEvent)")
+    return jsonEvent
   }
 
 }
